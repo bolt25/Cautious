c7d3348a6d648271bb07cae9d304a092b13c179b
bolt25 <dharmik255@gmail.com>
2020-02-08 10:29:38 +0530

    Changed main screen UI

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,33 +1,75 @@
 import 'package:flutter/material.dart';
-
+//
+//
+//
 void main() => runApp(MaterialApp(
-  home: Home())
+  home: HomeScreen())
 
 );
 
 
-class Home extends StatelessWidget {
+
+class HomeScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Cautious!'),
-        centerTitle: true,
-      ),
-      body: Center(
-          child: Image(
-            image: AssetImage('images/Cautious!.png'),
-          ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('Find a Routed',
-          style: TextStyle(
-              fontSize: 15.0,
-              fontWeight: FontWeight.bold
-          ),),
-      ),
-    );
+  State<StatefulWidget> createState() {
+    return new HomeScreenState();
   }
 }
 
+class HomeScreenState extends State<HomeScreen> {
+  int number = 0;
+
+  void dangerAlert() {
+    setState(() {
+      number = number - 1;
+    });
+  }
+
+  void findRoute() {
+    setState(() {
+      number = number + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        backgroundColor: Colors.black,
+        appBar: new AppBar(
+          title: new Text(""),
+          backgroundColor: Colors.black,
+        ),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                image: AssetImage('images/Cautious.png'),
+                fit: BoxFit.cover,
+              ),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new RaisedButton(
+                    padding: const EdgeInsets.all(8.0),
+                    textColor: Colors.white,
+                    color: Colors.blue,
+                    onPressed: findRoute,
+                    child: new Text("Find me a route!"),
+                  ),
+                  new RaisedButton(
+                    onPressed: dangerAlert,
+                    textColor: Colors.white,
+                    color: Colors.red,
+                    padding: const EdgeInsets.all(8.0),
+                    child: new Text(
+                      "Danger Alert!",
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+}
